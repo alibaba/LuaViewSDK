16389b5c0d5604897fda9a896e106ad1988b608c
城西 <xicheng.dxc@alibaba-inc.com>
2015-09-23 17:35:42 +0800

    demo例子更新

--- IOS/Demo/lua/demoA_UIImageView.lua
+++ IOS/Demo/lua/demoA_UIImageView.lua
@@ -4,10 +4,12 @@ print("初始化窗口大小, 默认窗口大小是(0,0)");
 w,h = System.screenSize();
 window.setFrame(0, 0, w, h);
 window.setUserInteractionEnabled(true);
-window.setBackgroundColor(0xffffff,1);
+window.setBackgroundColor(0x777777,1);
+
+Y0 = 100
 
 imageView = UIImageView();
-imageView.setFrame(0,30,100,100);
+imageView.setFrame(30, Y0, 100, 100);
 imageView.setBackgroundColor(0xff0000,1);
 names = {};
 for i=1,16 do
@@ -15,3 +17,10 @@ for i=1,16 do
 end
 imageView.setImage("16.tiff");
 imageView.startAnimating( names, 3.0, 1);
+
+
+
+imageView2 = UIImageView();
+imageView2.setFrame(160, Y0, 100, 100);
+imageView2.setImage("http://g.alicdn.com/ju/lua/2.0.24/doc/icon.png");
+
