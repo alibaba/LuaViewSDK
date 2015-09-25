9f7f3bcb5e438610a489e318f05f708222c98e11
城西 <xicheng.dxc@alibaba-inc.com>
2015-09-25 11:54:50 +0800

    修改图片按钮和资源

--- LuaViewDebugger/src/com/alibaba/lvdebug/ui/ImageButton.java
+++ LuaViewDebugger/src/com/alibaba/lvdebug/ui/ImageButton.java
@@ -1,16 +1,20 @@
 package com.alibaba.lvdebug.ui;
 
 import java.awt.Dimension;
+import java.awt.Image;
 import java.awt.Insets;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
 public class ImageButton extends JButton {
+	private static final int BUT_H = 22;
 
 	private static final long serialVersionUID = -8171450113272087720L;
 
 	public ImageButton(ImageIcon icon, ImageIcon icon2) {
+		icon = changeIcon(icon);
+		icon2 = changeIcon(icon2);
 		setIcon(icon);
 		this.setDisabledIcon(icon2);
 		setMargin(new Insets(0, 20, 0, 10));// 将边框外的上下左右空间设置为0
@@ -20,7 +24,11 @@ public class ImageButton extends JButton {
 		setText(null);// 除去按钮的默认名称
 		setFocusPainted(false);// 除去焦点的框
 		setContentAreaFilled(false);// 除去默认的背景填充
-		setSize(24, 24);
-		this.setPreferredSize(new Dimension(24 + 30, 24));
+		this.setPreferredSize(new Dimension(BUT_H + 30, BUT_H));
+	}
+
+	public static ImageIcon changeIcon(ImageIcon ico) {
+		Image temp = ico.getImage().getScaledInstance(BUT_H, BUT_H, Image.SCALE_SMOOTH);
+		return new ImageIcon(temp);
 	}
 }
