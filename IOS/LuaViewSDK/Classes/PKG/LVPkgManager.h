26ab759ee7fb2061e2a576077d3d0a01ad7f0ba6
城西 <xicheng.dxc@alibaba-inc.com>
2015-10-09 17:25:49 +0800

    升级LuaView脚本不向下兼容了!!!!!!!!!!

--- IOS/LuaViewSDK/Classes/PKG/LVPkgManager.h
+++ IOS/LuaViewSDK/Classes/PKG/LVPkgManager.h
@@ -9,7 +9,7 @@
 #import <Foundation/Foundation.h>
 
 #define LUAVIEW_ROOT_PATH  @"LUAVIEW"
-#define LUAVIEW_VERSION   @"2.2.0"
+#define LUAVIEW_VERSION   @"3.0.0"
 
 #define LV_PKGINFO_PROPERTY_URL      @"url"
 #define LV_PKGINFO_PROPERTY_TIME     @"time"
