b852910df8789538c9c43f8a668ebc4f43cef56a
城西 <xicheng.dxc@alibaba-inc.com>
2015-10-23 16:28:59 +0800

    修改脚本语法不兼容

--- IOS/Demo/lua/demoG_TableView.lua
+++ IOS/Demo/lua/demoG_TableView.lua
@@ -12,51 +12,51 @@ function createTableView( )
 		Section = {
 			SectionCount = 2,
 
-			RowCount = function(section){
+			RowCount = function(section)
 				return 10;
-			}
+			end
 		},
 
 		Cell = {
-			Id = function( section, row ){
+			Id = function( section, row )
 				if ( section==1  ) then
 					return  "ImageAndLabel";
 				else
 					return "ImageAndImage";
 				end
-			},
+			end,
 			ImageAndLabel = {
-				Height = function( section, row ){
+				Height = function( section, row )
 					return cellHeight;
-				},
-				Init = function(cell, section, row){
+				end,
+				Init = function(cell, section, row)
 					cell.icon = Image();
 					cell.title = Label();
 					print("构造Cell");
 					print(cell.window);
-				},
-				Layout = function(cell, section, row){
+				end,
+				Layout = function(cell, section, row)
 					cell.icon.frame(0, 0, cellHeight, cellHeight);
 					cell.icon.image(imageUrl1);
 
 					cell.title.frame(cellHeight, 0, w-cellHeight, cellHeight);
 					cell.title.text("测试"..section .."--" .. row);
-				},
-				Callback = function( section, row ){
+				end,
+				Callback = function( section, row )
 					print(section, row);
 					tv.stopRefreshing();
 					System.gc();
-				}
+				end
 			},
 			ImageAndImage = {
-				Height = function( section, row ){
+				Height = function( section, row )
 					return cellHeight;
-				},
-				Init = function(cell, section, row){
+				end,
+				Init = function(cell, section, row)
 					cell.icon = Image();
 					cell.icon2 = Image();
-				},
-				Layout = function(cell, section, row){
+				end,
+				Layout = function(cell, section, row)
 					print(window);
 					local cellWdith, cellHeight = cell.window.size();
 					cell.icon.frame(0, 0, cellHeight, cellHeight);
@@ -65,38 +65,39 @@ function createTableView( )
 
 					cell.icon2.frame(cellHeight+10, 0, cellHeight, cellHeight);
 					cell.icon2.image(imageUrl1);
-				},
-				Callback = function( section, row ){
+				end,
+				Callback = function( section, row )
 					print(tv, section, row);
 					tv.stopRefreshing();
 					System.gc();
-				}
+				end
+
 			}
 		},
-		Header = function(){
+		Header = function()
 			local head = Button();
 			head.title("我是头");
 			head.frame(0,0,w,100);
 			head.backgroundColor(0xff);
 			return head;
-		},
-		Footer = function(){
+		end,
+		Footer = function()
 			local foot = Button();
 			foot.title("我是结尾");
 			foot.frame(0,0,w,100);
 			foot.backgroundColor(0xff0000);
 			return foot;
-		},
+		end,
 		Callback = {
-			Scrolling = function(){
+			Scrolling = function()
 				print("scrolling");
-			},
-			ScrollBegin = function(){
+			end,
+			ScrollBegin = function()
 				print("scrolling begin");
-			},
-			ScrollEnd = function(){
+			end,
+			ScrollEnd = function()
 				print("scrolling end");
-			}
+			end
 		}
 	};
 	return tv;
