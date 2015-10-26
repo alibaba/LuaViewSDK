fce6958864a137f0653cfda5b1a6b4a28ab70623
城西 <xicheng.dxc@alibaba-inc.com>
2015-10-26 17:32:44 +0800

    整理代码

--- IOS/LuaViewSDK/Classes/lvsdk/LVNavigation.m
+++ IOS/LuaViewSDK/Classes/lvsdk/LVNavigation.m
@@ -8,22 +8,34 @@
 
 #import "LVNavigation.h"
 #import "LView.h"
+#import "LVStyledString.h"
 
 @implementation LVNavigation
 
 
 static int setTitle (lv_State *L) {
     lv_clearFirstTableValue(L);
-        LView* lview = (__bridge LView *)(L->lView);
-        UIViewController* vc = lview.viewController;
-        if( lv_gettop(L)>=1 ) {
+    LView* lview = (__bridge LView *)(L->lView);
+    UIViewController* vc = lview.viewController;
+    if( vc && lv_gettop(L)>=1 ) {
+        if( lv_type(L, 1)== LV_TSTRING ) {// string
+            vc.navigationItem.title = lv_paramString(L, 1);
+            return 0;
+        } else if( lv_type(L, 1)== LV_TUSERDATA ) {// 支持复合文本
+            LVUserDataStyledString * user2 = lv_touserdata(L, 1);
+            if( user2 && LVIsType(user2, LVUserDataStyledString) ) {
+                UILabel* label = [[UILabel alloc] init];
+                LVStyledString* attString = (__bridge LVStyledString *)(user2->styledString);
+                [label setAttributedText:attString.mutableStyledString];
+                vc.navigationItem.titleView = label;
+                return 0;
+            }
+        }
         id object = lv_luaValueToNativeObject(L, 1);
-        if ( [object isKindOfClass:[NSString class]] ) {
-            vc.navigationItem.title = object;
-        } else if([object isKindOfClass:[UIView class]] ) {
+        if([object isKindOfClass:[UIView class]] ) {// View
             vc.navigationItem.titleView = object;
         }
-        return 0; /* number of results */
+        return 0;
     }
     return 0;
 }
@@ -40,22 +52,22 @@ static int setTitle (lv_State *L) {
     return array;
 }
 
-static int setLeftBarButton (lv_State *L) {
+static int setLeftButton (lv_State *L) {
     lv_clearFirstTableValue(L);
     LView* lview = (__bridge LView *)(L->lView);
     UIViewController* vc = lview.viewController;
-    if( vc ) {
+    if( vc && lv_gettop(L)>=1 ) {
         NSArray* buttonItems = [LVNavigation getNavigationItems:L];
         vc.navigationItem.leftBarButtonItems = buttonItems;
     }
     return 0; /* number of results */
 }
 
-static int setRightBarButton (lv_State *L) {
+static int setRightButton (lv_State *L) {
     lv_clearFirstTableValue(L);
     LView* lview = (__bridge LView *)(L->lView);
     UIViewController* vc = lview.viewController;
-    if( vc ) {
+    if( vc && lv_gettop(L)>=1 ) {
         NSArray* buttonItems = [LVNavigation getNavigationItems:L];
         vc.navigationItem.rightBarButtonItems = buttonItems;
     }
@@ -66,9 +78,9 @@ static int setBackground(lv_State*L ) {
     lv_clearFirstTableValue(L);
     LView* lview = (__bridge LView *)(L->lView);
     UIViewController* vc = lview.viewController;
-    if( vc ) {
+    if( vc && lv_gettop(L)>=1 ) {
         UINavigationBar* navBar = vc.navigationController.navigationBar;
-        
+
         id obj = lv_luaValueToNativeObject(L, 1);
         if( [obj isKindOfClass:[UIImageView class]] ) {
             UIImageView* imgView = obj;
@@ -77,23 +89,7 @@ static int setBackground(lv_State*L ) {
             if( image.scale<scale) {
                 image = [UIImage imageWithCGImage:image.CGImage scale:scale orientation:0];
             }
-            [navBar setBackgroundImage:image  forBarMetrics:UIBarMetricsDefault];
-        }
-        if ( lv_type(L, 2) ==LV_TNUMBER ) {
-            NSUInteger color = lv_tonumber(L, 2);
-            float a = ( (color>>24)&0xff )/255.0;
-            float r = ( (color>>16)&0xff )/255.0;
-            float g = ( (color>>8)&0xff )/255.0;
-            float b = ( (color>>0)&0xff )/255.0;
-            if( a==0 ){
-                a = 1;
-            }
-            if( lv_gettop(L)>=3 && lv_type(L, 3) ==LV_TNUMBER){
-                a = lv_tonumber(L, 3);
-            }
-            UIColor* c  = [UIColor colorWithRed:r green:g blue:b alpha:a];
-            [navBar setTintColor:c];
-            [navBar setBarTintColor:c];
+            [navBar setBackgroundImage:image forBarMetrics:UIBarMetricsDefault];
         }
     }
     return 0;
@@ -101,22 +97,43 @@ static int setBackground(lv_State*L ) {
 
 static int setStatusBarStyle (lv_State *L) {
     lv_clearFirstTableValue(L);
-    NSInteger value = lv_tonumber(L, 2);
-    [[UIApplication sharedApplication] setStatusBarStyle:value];
-    return 0; /* number of results */
+    if ( lv_gettop(L)>=1 ) {
+        NSInteger value = lv_tonumber(L, 2);
+        [[UIApplication sharedApplication] setStatusBarStyle:value];
+    }
+    return 0;
 }
 
 +(int) classDefine:(lv_State *)L{
-    const struct lvL_reg staticFunctions [] = {
+    const struct lvL_reg fs [] = {
         {"title", setTitle},
-        {"leftBarButton", setLeftBarButton},
-        {"rightBarButton", setRightBarButton},
+        {"leftBarButton", setLeftButton},
+        {"rightBarButton", setRightButton},
         {"background", setBackground},
         {"statusBarStyle", setStatusBarStyle},
         {NULL, NULL}
     };
-    lvL_openlib(L, "Navigation", staticFunctions, 0);
+    lvL_openlib(L, "Navigation", fs, 0);
     return 0;
 }
 
 @end
+
+//        if ( lv_type(L, 2) ==LV_TNUMBER ) {
+//            NSUInteger color = lv_tonumber(L, 2);
+//            float a = ( (color>>24)&0xff )/255.0;
+//            float r = ( (color>>16)&0xff )/255.0;
+//            float g = ( (color>>8)&0xff )/255.0;
+//            float b = ( (color>>0)&0xff )/255.0;
+//            if( a==0 ){
+//                a = 1;
+//            }
+//            if( lv_gettop(L)>=3 && lv_type(L, 3) ==LV_TNUMBER){
+//                a = lv_tonumber(L, 3);
+//            }
+//            UIColor* c  = [UIColor colorWithRed:r green:g blue:b alpha:a];
+//            [navBar setTintColor:c];
+//            [navBar setBarTintColor:c];
+//        }
+
+
