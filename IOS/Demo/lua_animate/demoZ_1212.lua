be0273dce2fc172f1c4d736b41da6784676fe5e3
城西 <xicheng.dxc@alibaba-inc.com>
2015-12-03 20:46:47 +0800

    调整红包雨脚本

--- IOS/Demo/lua_animate/demoZ_1212.lua
+++ IOS/Demo/lua_animate/demoZ_1212.lua
@@ -3,8 +3,13 @@ SCR_W, SCR_H = System.screenSize();
 
 
 window.frame(0, 0, SCR_W, SCR_H);
-window.backgroundColor(0,0.5);
+window.backgroundColor(0);
 window.enabled(true);
+
+bg = Image();
+bg.image("jhs.png");
+bg.frame(0,0,SCR_W, SCR_H);
+bg.alpha(0.5)
 
 -------------------------------
 function snowCreater(fileName, x0, y0)
@@ -19,7 +24,7 @@ function snowCreater(fileName, x0, y0)
 	local imageW,imageH = snow.imageView.size();
 	snow.imageView.size(imageW/3, imageH/3);
 
-	snow.imageView.center(x0,y0);
+	snow.imageView.center( SCR_W + x0, y0 );
 
 	function snow.move()
 		self.imageView.center(self.x,self.y);
@@ -28,27 +33,19 @@ function snowCreater(fileName, x0, y0)
 		-- self.imageView.transform3D(t);
 	end
 	function snow.nextXY()
-		local dx = -SCR_W*1.1;
-		local dy = SCR_H*1.1;
 		local x,y = self.imageView.center();
+		local dx = -x-60;
+		local dy = (x + 60) * SCR_H/SCR_W;
+
 		self.x = x+dx;
 		self.y = y+dy;
-		self.rote = math:random(10,90)/100.0*3.14;
-		self.scale = math:random(2,10)/10;
+		self.time = math:sqrt(dx*dx + dy*dy)/ math:sqrt(SCR_W*SCR_W+SCR_H*SCR_H) * 2.5;
+		-- self.rote = math:random(10,90)/100.0*3.14;
+		-- self.scale = math:random(2,10)/10;
 	end
 	function snow.showSnows()
-		if ( self.times>5 ) then
-			diedNum = diedNum - 1;
-			print(diedNum);
-			if( diedNum == 1 ) then
-				closeApp();
-			end
-			return ;
-		end
-		self.times = self.times + 1;
-
 		self.nextXY();
-		local time = math:random(20,30)/10.0;
+		local time = math:random(0.8,1)*self.time;
 		Animate(time,0,0,0,3,
 			function()
 				self.move();
@@ -71,17 +68,18 @@ end
 -------------------------------------
 snowArr = {};
 index = 1;
-DXY = 100
-for i=1, 6 do
-	for j=1, 6 do
-		local dx = -DXY*3+i*DXY;
-		local dy = -DXY*7 + j*DXY;
+DXY = 80
+NUM = 9
+for i=1, NUM do
+	for j=1, NUM do
+		local dx = -DXY*NUM/2+i*DXY;
+		local dy = -DXY*(NUM+1) + j*DXY;
 		if( index%2==0 ) then
-			if( math:random(0,10)>6 ) then
-				snowArr[index] = snowCreater("line.png", SCR_W + dx, dy ).showSnows();
+			if( math:random(0,10)>4 ) then
+				snowArr[index] = snowCreater("line.png", dx, dy ).showSnows();
 			end
 		else
-			snowArr[index] = snowCreater("hongbao.png",SCR_W +dx, dy).showSnows();
+			snowArr[index] = snowCreater("hongbao.png",dx, dy).showSnows();
 		end
 		index = index+1;
 	end
@@ -90,16 +88,17 @@ end
 
 ------------
 function showEnterButton()
+	local small = SCR_W/375;
 	enterButton = Image();
 	enterButton.image("enter.png");
 	enterButton.adjustSize();
 	local imageW,imageH = enterButton.size();
-	enterButton.size(imageW/3, imageH/3);
+	enterButton.size(imageW/3*small, imageH/3*small);
 
 	enterButton.callback(function ()
 		viewController.openUrl("http://taoboa.com");
 	end);
-	enterButton.center( SCR_W/2, SCR_H/2 );
+	enterButton.center( SCR_W*0.5, SCR_H*0.4 );
 	enterButton.scale( 0, 0)
 	Animate( 1, 0, 0.5,0,
 		function(){
@@ -113,7 +112,7 @@ function showEnterButton()
 	closeButton.image("close.png");
 	closeButton.adjustSize();
 	local imageW,imageH = closeButton.size();
-	closeButton.size(imageW/3, imageH/3);
+	closeButton.size(imageW/3*small, imageH/3*small);
 	closeButton.bottom(enterButton.top());
 	closeButton.right(enterButton.right());
 	closeButton.callback(function ()
