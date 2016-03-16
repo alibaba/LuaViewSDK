2b3e52046af0fc6205e87b2ab4dc40f736a28d87
城西 <xicheng.dxc@alibaba-inc.com>
2016-03-16 16:06:19 +0800

    doc: LuaViewSDK  标准文档更新

--- Doc/LuaViewSDK/AudioPlayer.lua
+++ Doc/LuaViewSDK/AudioPlayer.lua
@@ -5,6 +5,18 @@
 --- 创建声音播放器
 -- @string url 声音资源(支持本地文件和网络资源)
 -- @treturn AudioPlayer player 播放器
+-- @usage 
+-- w,h = System.screenSize();
+-- player = AudioPlayer("http://g.tbcdn.cn/ju/lua/1.2.0/shake.js");
+-- button = Button();
+-- button.title("播放音效");
+-- button.frame(0,100,w,40);
+-- button.backgroundColor(0xff,1);
+-- button.callback( function()
+-- 	if( player ) then
+-- 		player.play();
+-- 	end
+-- end)
 function AudioPlayer()
 end
 
--- Doc/LuaViewSDK/CustomPanel.lua
+++ Doc/LuaViewSDK/CustomPanel.lua
@@ -15,10 +15,23 @@ end
 
 
 
- --- 设置callback
- -- @tparam function callback 代理方法
- -- @usage errorView.callback( function(){
- --     -- code
- -- } );
+--- 设置callback
+-- @tparam function callback 代理方法
+-- @usage 
+-- scrW,scrH = System.screenSize();
+-- x0 = scrW/6;
+-- y0 = scrH/10;
+-- w = scrW - x0*2;
+-- h = scrH / 3;
+-- customError = CustomPanel( "CustomError", x0,y0,w,h );
+-- customError.backgroundColor(0xff0000);
+-- customError.callback( function(tag)
+--     print("customError", tag);
+-- end);
+-- customError2 = CustomError(x0,y0*2 + h,w,h );
+-- customError2.backgroundColor(0xff0000);
+-- customError2.callback( function(tag)
+--     print("customError2", tag);
+-- end)
 function callback()
 end
