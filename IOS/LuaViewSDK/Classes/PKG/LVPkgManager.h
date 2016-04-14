5544d4f5ad1a5d13ba95fdbc6f5adfb8a88b81eb
聂商 <yubo.dyb@alibaba-inc.com>
2016-04-14 14:22:51 +0800

    修改package存放路径

--- IOS/LuaViewSDK/Classes/PKG/LVPkgManager.h
+++ IOS/LuaViewSDK/Classes/PKG/LVPkgManager.h
@@ -8,7 +8,7 @@
 
 #import <Foundation/Foundation.h>
 
-#define LUAVIEW_ROOT_PATH  @"LUAVIEW500"
+#define LUAVIEW_ROOT_PATH  @"LUAVIEW_500"
 #define LUAVIEW_VERSION   "5.0.0"
 
 #define LV_PKGINFO_PROPERTY_URL      @"url"
