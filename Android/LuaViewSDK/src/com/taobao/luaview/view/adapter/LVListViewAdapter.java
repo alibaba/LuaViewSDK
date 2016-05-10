60cd2345e3f5f43c60af30df44f842443bdff4ca
野松 <songtao.cst@alibaba-inc.com>
2016-05-10 10:50:03 +0800

    bugfix: 修复ListViewAdapter cell重用的bug

--- Android/LuaViewSDK/src/com/taobao/luaview/view/adapter/LVListViewAdapter.java
+++ Android/LuaViewSDK/src/com/taobao/luaview/view/adapter/LVListViewAdapter.java
@@ -21,7 +21,6 @@ import org.luaj.vm2.LuaValue;
  * @date 15/8/31
  */
 public class LVListViewAdapter extends BaseAdapter {
-
     private static final String KEY_VIEW_TYPE = "_lv_key_view_type";
     private UDBaseListView mLuaUserData;
     private Globals mGlobals;
