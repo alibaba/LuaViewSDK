721ec3eda1855cbd4b97e2377d6a4601ab729daf
野松 <songtao.cst@alibaba-inc.com>
2016-05-17 20:54:34 +0800

    request update: 修改获取图片

--- Android/LuaViewSDK/src/com/taobao/luaview/view/imageview/LVBaseImageView.java
+++ Android/LuaViewSDK/src/com/taobao/luaview/view/imageview/LVBaseImageView.java
@@ -1,6 +1,9 @@
 package com.taobao.luaview.view.imageview;
 
+import android.app.Activity;
+import android.app.Application;
 import android.content.Context;
+import android.os.Bundle;
 import android.view.View;
 
 import com.taobao.luaview.global.LuaView;
@@ -17,6 +20,48 @@ public class LVBaseImageView extends BaseImageView {
 
     public LVBaseImageView(Context context) {
         super(context);
+        initRecycler(context);
+    }
+
+    private void initRecycler(Context context) {
+        if(context instanceof Activity){
+            ((Activity) context).getApplication().registerActivityLifecycleCallbacks(new Application.ActivityLifecycleCallbacks() {
+                @Override
+                public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+
+                }
+
+                @Override
+                public void onActivityStarted(Activity activity) {
+                    restoreImage();
+                }
+
+                @Override
+                public void onActivityResumed(Activity activity) {
+
+                }
+
+                @Override
+                public void onActivityPaused(Activity activity) {
+
+                }
+
+                @Override
+                public void onActivityStopped(Activity activity) {
+                    releaseBitmap();
+                }
+
+                @Override
+                public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
+
+                }
+
+                @Override
+                public void onActivityDestroyed(Activity activity) {
+                    ((Activity) getContext()).getApplication().unregisterActivityLifecycleCallbacks(this);
+                }
+            });
+        }
     }
 
     @Override
@@ -33,17 +78,17 @@ public class LVBaseImageView extends BaseImageView {
         return mUrl;
     }
 
-    @Override
-    protected void onWindowVisibilityChanged(int visibility) {
-        super.onWindowVisibilityChanged(visibility);
-        if (isNetworkMode) { // 清空内存
-            if (visibility == View.VISIBLE && mAttachedWindow) {
-                restoreImage();
-            } else {
-                releaseBitmap();
-            }
-        }
-    }
+//    @Override
+//    protected void onWindowVisibilityChanged(int visibility) {
+//        super.onWindowVisibilityChanged(visibility);
+//        if (isNetworkMode) { // 清空内存
+//            if (visibility == View.VISIBLE && mAttachedWindow) {
+//                restoreImage();
+//            } else {
+//                releaseBitmap();
+//            }
+//        }
+//    }
 
     private void restoreImage() {// 恢复被清空的image
         if (isNetworkMode) {
