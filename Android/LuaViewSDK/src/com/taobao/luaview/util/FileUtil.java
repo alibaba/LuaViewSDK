518ef06d3a9dcac1206036ea3b9e42b5ba6df473
野松 <songtao.cst@alibaba-inc.com>
2016-08-18 19:35:58 +0800

    request update: delete files

--- Android/LuaViewSDK/src/com/taobao/luaview/util/FileUtil.java
+++ Android/LuaViewSDK/src/com/taobao/luaview/util/FileUtil.java
@@ -339,6 +339,7 @@ public class FileUtil {
                         delete(child);
                     }
                 }
+                file.delete();
             } else {
                 file.delete();
             }
