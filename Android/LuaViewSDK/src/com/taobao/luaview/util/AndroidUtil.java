acd79cada304fdea60a6959c5fb66ad80e2f07ce
野松 <songtao.cst@alibaba-inc.com>
2016-09-08 16:25:24 +0800

    request update: 修复颜色bug

--- Android/LuaViewSDK/src/com/taobao/luaview/util/AndroidUtil.java
+++ Android/LuaViewSDK/src/com/taobao/luaview/util/AndroidUtil.java
@@ -113,6 +113,7 @@ public class AndroidUtil {
      * @return
      */
     public static float getDensity(Context context) {
+        context.getApplicationContext().getResources().getConfiguration().orientation;
         return context.getApplicationContext().getResources().getDisplayMetrics().density;
     }
 
