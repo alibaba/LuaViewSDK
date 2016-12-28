f04ace2eb3a825d894129e442e763f0488cb1dfe
野松 <songtao.cst@alibaba-inc.com>
2016-12-28 11:19:29 +0800

    request update: update code

--- Android/LuaViewSDK/github_sync.sh
+++ Android/LuaViewSDK/github_sync.sh
@@ -1,23 +1,33 @@
 GIT_MERGE="git merge $1"
 
-echo "current branch is: "
+echo "save current branch changes:"
+
+git stash
 
 GIT_CURRENT_BRANCH=$(git symbolic-ref -q HEAD)
 GIT_CURRENT_BRANCH=${GIT_CURRENT_BRANCH##refs/heads/}
 GIT_CURRENT_BRANCH=${GIT_CURRENT_BRANCH:-HEAD}
 
+
 git pull
 git checkout develop
 git pull
 $GIT_MERGE
+
+echo "\npush to develop"
 git push
+echo "\npush to github on branch develop"
 git push https://github.com/alibaba/LuaViewSDK develop:develop
 
+echo "\n"
 git checkout master
 git pull
 git merge develop
+
+echo "\npush to master"
 git push
+echo "\npush to github on branch master"
 git push https://github.com/alibaba/LuaViewSDK master:master
 
 git checkout $GIT_CURRENT_BRANCH
-
+git stash apply
