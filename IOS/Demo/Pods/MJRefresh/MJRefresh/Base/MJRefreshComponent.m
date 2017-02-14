8acaa7e16d6c1a3ea8482b8bd3ffe43d6f39c5b1
城西 <xicheng.dxc@alibaba-inc.com>
2017-02-14 13:59:49 +0800

    修复crash

--- IOS/Demo/Pods/MJRefresh/MJRefresh/Base/MJRefreshComponent.m
+++ IOS/Demo/Pods/MJRefresh/MJRefresh/Base/MJRefreshComponent.m
@@ -147,7 +147,9 @@
         // 如果是iOS9以上，截取前面的语言标识
         if ([UIDevice currentDevice].systemVersion.floatValue >= 9.0) {
             NSRange range = [language rangeOfString:@"-" options:NSBackwardsSearch];
-            language = [language substringToIndex:range.location];
+            if( range.length>0 ) {
+                language = [language substringToIndex:range.location];
+            }
         }
         
         if (language.length == 0) {
