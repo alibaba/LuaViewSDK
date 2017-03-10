acbcf878c367fac74d64e876230934be31b016bd
城西 <xicheng.dxc@alibaba-inc.com>
2017-03-10 11:38:46 +0800

    兼容性API

--- IOS/SDK/LuaViewSDK/Classes/LView.h
+++ IOS/SDK/LuaViewSDK/Classes/LView.h
@@ -12,5 +12,10 @@
 #pragma mark -LuaView 类加载脚本调用接口
 @interface LView : LuaView
 
+@property (nonatomic,assign) BOOL changeGrammar;// 是否需要语法转换（原先luaview语法和lua标准语法的区别是‘.’和':'互换了），默认是非标准语法，需要转换
+
+@property (nonatomic,assign) BOOL checkDebugerServer; // 是否检查调试器
+
+
 @end
 
--- IOS/SDK/LuaViewSDK/Classes/LView.m
+++ IOS/SDK/LuaViewSDK/Classes/LView.m
@@ -4,4 +4,21 @@
 
 @implementation LView
 
+
+-(BOOL) changeGrammar{
+    return self.luaviewCore.changeGrammar;
+}
+
+-(void) setChangeGrammar:(BOOL)changeGrammar{
+    self.luaviewCore.changeGrammar = changeGrammar;
+}
+
+-(BOOL) checkDebugerServer{
+    return self.luaviewCore.checkDebugerServer;
+}
+
+-(void) setCheckDebugerServer:(BOOL)checkDebugerServer{
+    self.luaviewCore.checkDebugerServer = checkDebugerServer;
+}
+
 @end
