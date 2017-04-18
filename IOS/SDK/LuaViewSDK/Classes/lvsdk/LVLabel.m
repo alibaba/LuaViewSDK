ee83e2e8f8beff64bd372fbce0d4d84a93e35b84
城西 <xicheng.dxc@alibaba-inc.com>
2017-04-18 14:30:03 +0800

    [IOS] Label注释说明

--- IOS/SDK/LuaViewSDK/Classes/lvsdk/LVLabel.m
+++ IOS/SDK/LuaViewSDK/Classes/lvsdk/LVLabel.m
@@ -20,12 +20,12 @@
 -(id) init:(NSString*)imageName l:(lua_State*) l{
     self = [super init];
     if( self ){
-        self.lv_luaviewCore = LV_LUASTATE_VIEW(l);
-        self.text = imageName;
-        self.backgroundColor = [UIColor clearColor];
-        self.textAlignment = NSTextAlignmentLeft;
-        self.lv_isCallbackAddClickGesture = YES;
-        self.clipsToBounds = YES;
+        self.lv_luaviewCore = LV_LUASTATE_VIEW(l);// 获取luaview运行内核
+        self.text = imageName;// 初始化Label初始text
+        self.backgroundColor = [UIColor clearColor];// 默认背景色是透明色
+        self.textAlignment = NSTextAlignmentLeft;// 默认是左对齐
+        self.lv_isCallbackAddClickGesture = YES;// 默认允许添加事件监听
+        self.clipsToBounds = YES;// 默认出界不可见
         self.font = [UIFont systemFontOfSize:14];// 默认字体大小
     }
     return self;
@@ -36,25 +36,26 @@
 
 #pragma -mark UILabel
 /*
- * lua脚本中Label() 对应的构造方法
+ * lua脚本中 local label = Label() 对应的构造方法
  */
 static int lvNewLabel(lua_State *L) {
+    // 获取构造方法对应的Class(Native类)
     Class c = [LVUtil upvalueClass:L defaultClass:[LVLabel class]];
     {
-        NSString* text = lv_paramString(L, 1);// 5
-        LVLabel* label = [[c alloc] init:text l:L];
+        NSString* text = lv_paramString(L, 1);// 获取脚本传过来的第一个参数(约定是字符串类型)
+        LVLabel* label = [[c alloc] init:text l:L];//通过Class和参数构造脚本对应的真实实例
         
         {
-            NEW_USERDATA(userData, View);
-            userData->object = CFBridgingRetain(label);
-            label.lv_userData = userData;
+            NEW_USERDATA(userData, View);// 创建lua对象(userdata)
+            userData->object = CFBridgingRetain(label);// 脚本对象引用native对象
+            label.lv_userData = userData;//native对象引用脚本对象
             
-            luaL_getmetatable(L, META_TABLE_UILabel );
-            lua_setmetatable(L, -2);
+            luaL_getmetatable(L, META_TABLE_UILabel ); // 获取Label对应的类方法列表
+            lua_setmetatable(L, -2); // 设置刚才创建的lua对象的方法列表是类Label的方法列表
         }
-        LuaViewCore* view = LV_LUASTATE_VIEW(L);
+        LuaViewCore* view = LV_LUASTATE_VIEW(L);// 获取当前LuaView对应的LuaViewCore
         if( view ){
-            [view containerAddSubview:label];
+            [view containerAddSubview:label]; // 把label对象加到LuaViewCore里面
         }
     }
     return 1; /* new userdatum is already on the stack */
@@ -64,22 +65,26 @@ static int lvNewLabel(lua_State *L) {
  * 脚本label实例对象label.text()方法对应的Native实现
  */
 static int text (lua_State *L) {
-    LVUserDataInfo * user = (LVUserDataInfo *)lua_touserdata(L, 1);
+    LVUserDataInfo * user = (LVUserDataInfo *)lua_touserdata(L, 1);// 获取第一个参数永远是self(lua的userdata)
     if( user ) {
-        LVLabel* view = (__bridge LVLabel *)(user->object);
-        if ( [view isKindOfClass:[LVLabel class]] ) {
-            if( lua_gettop(L)>=2 ) {
+        LVLabel* view = (__bridge LVLabel *)(user->object);// 当前userdata对应的native对象
+        if ( [view isKindOfClass:[LVLabel class]] ) {// 检查类型是否匹配(其实可以不用检查一般一定是对的)
+            if( lua_gettop(L)>=2 ) {// 检查是否参数大于等于两个(self+文本), 如果只要一个是取值操作, 两个是赋值操作
                 if ( lua_isnoneornil(L, 2 ) ) {
+                    // 如果参数的值是nil标识清空text操作
                     view.text = nil;
                 } else if( lua_type(L, 2)==LUA_TNUMBER ){
-                    CGFloat text = lua_tonumber(L, 2);// 2
+                    // 如果参数数值, 转换成字符串再赋值
+                    CGFloat text = lua_tonumber(L, 2);// 获取第二个参数, 第二个参数实际上对应脚本中的第一个参数
                     view.text = [NSString stringWithFormat:@"%f",text];
                     return 0;
                 } else if( lua_type(L, 2)==LUA_TSTRING ){
-                    NSString* text = lv_paramString(L, 2);// 2
+                    // 如果是字符串, 直接赋值
+                    NSString* text = lv_paramString(L, 2);// 获取第二个参数, 第二个参数实际上对应脚本中的第一个参数
                     view.text = text;
                     return 0;
                 } else if( lua_type(L, 2)==LUA_TUSERDATA ){
+                    // 如果是复合文本, 则获取复合文本对应的NSAttributedString后把复合文本赋值给Label
                     LVUserDataInfo * user2 = lua_touserdata(L, 2);// 2
                     if( user2 && LVIsType(user2, StyledString) ) {
                         LVLabel* view = (__bridge LVLabel *)(user->object);
@@ -89,6 +94,7 @@ static int text (lua_State *L) {
                     }
                 }
             } else {
+                // 取值操作
                 NSString* text = view.text;
                 lua_pushstring(L, text.UTF8String);
                 return 1;
@@ -102,18 +108,19 @@ static int text (lua_State *L) {
  * 脚本label实例对象label.lineCount()方法对应的Native实现
  */
 static int lineCount(lua_State *L) {
-    LVUserDataInfo * user = (LVUserDataInfo *)lua_touserdata(L, 1);
+    LVUserDataInfo * user = (LVUserDataInfo *)lua_touserdata(L, 1);// 获取第一个参数永远是self(lua的userdata)
     if( user ){
-        LVLabel* view = (__bridge LVLabel *)(user->object);
-        if( lua_gettop(L)>=2 ) {
-            int number = lua_tonumber(L, 2);// 2
-            if( [view isKindOfClass:[LVLabel class]] ){
-                view.numberOfLines = number;
+        LVLabel* view = (__bridge LVLabel *)(user->object);// 获取userdata对应的native对象
+        if( lua_gettop(L)>=2 ) {// 检查参数大于等于两个(self+lineCount), 如果只要一个是取值操作, 两个是赋值操作
+            int number = lua_tonumber(L, 2);// 获取第二个参数, 第二个参数实际上对应脚本中的第一个参数
+            if( [view isKindOfClass:[LVLabel class]] ){// 类型检查可以不做
+                view.numberOfLines = number;// 设置文本的行数
                 return 0;
             }
         } else {
-            lua_pushnumber(L, view.numberOfLines );
-            return 1;
+            // 如果参数个数小于两个说明是取值, 返回参数个数
+            lua_pushnumber(L, view.numberOfLines );// 返回文本的行数
+            return 1;// 返回参数个数
         }
     }
     return 0;
@@ -268,7 +275,7 @@ static int ellipsize (lua_State *L) {
  * luaview所有扩展类的桥接协议: 只是一个静态协议, luaview统一调用该接口加载luaview扩展的类
  */
 +(int) lvClassDefine:(lua_State *)L globalName:(NSString*) globalName{
-    // 注册lua的构造方法: "Label" 对应的closure 是 "lvNewLabel"
+    // 注册构造方法: "Label" 对应的C函数(lvNewLabel) + 对应的类Class(self/LVLabel)
     [LVUtil reg:L clas:self cfunc:lvNewLabel globalName:globalName defaultName:@"Label"];
     
     // lua Labe构造方法创建的对象对应的方法列表
